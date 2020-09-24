af4577834f35de265a90d4fc002b1645b33a730a
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-09-24 09:59:13 +0530

    Added Prime.java program

---
+++ Prime.java
@@ -0,0 +1,29 @@
+//Program to find nth prime number after a number x
+import java.util.*;
+public class Prime {
+    public static void main(String[] args) {
+ 
+        Scanner s = new Scanner(System.in);
+    	System.out.print("Enter n to compute the nth prime number: ");
+    	int n = s.nextInt();
+    	System.out.print("Enter x: ");
+    	int x = s.nextInt();
+    
+    	int num, count, i;
+    	num=x;
+    	count=0;
+ 
+    	while (count < n){
+      		num=num+1;
+      		for (i = 2; i <= num; i++){
+        		if (num % i == 0) {
+          		break;
+        		}
+      		}
+      	if ( i == num){
+       		count = count+1;
+     	}
+    	}
+    System.out.println("Value of nth prime: " + num);
+  }
+}
