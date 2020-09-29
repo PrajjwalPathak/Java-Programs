285dcd0f628af53cd3cc6db4df8df27fd00d903a
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-09-29 08:59:31 +0530

    Added ArrayInput.java program

---
+++ ArrayInput.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class ArrayInput {
+public static void main(String args[]) {
+    int[] z = new int[10];
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Enter 10 integers: ");
+
+    for(int i=0;i<10;i++) {
+        z[i] = s.nextInt();
+    }
+
+    System.out.print("The Array is: ");
+
+    for(int i=0;i<10;i++) {
+        System.out.print(" "+z[i]);
+    }
+  }
+}
