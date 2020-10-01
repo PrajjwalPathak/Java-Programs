4d4241b30123f9b31a68afea0db8f5202bab1710
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-10-01 09:32:41 +0530

    Added Palindrome.java program

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Palindrome {
+    public static void main(String args[]) {
+        int n, t, r = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        n = s.nextInt();
+        t = n;
+        while(t!=0) {
+            r=r*10+t%10;
+            t=t/10;
+        }
+        System.out.println("Reverse is: "+r);
+        
+        if(n == r) {
+            System.out.print("Number is a Palindrome");
+        }
+        else {
+            System.out.print("Number isn't a Palindrome");
+        }
+    }
+}
