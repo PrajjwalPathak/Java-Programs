abcdd9b2bb02299ec47369b0cffc90e858fcefeb
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-10-03 14:23:48 +0530

    Updated ArrayDublicate.java program

--- ArrayDublicate.java
+++ ArrayDublicate.java
@@ -1,21 +1,22 @@
+//Program to find the number of dublicate elements in an array
 import java.util.*; 
 
 class ArrayDublicate {
-  public static void main(String args[]) {
-    int[] z = new int[10];
+public static void main(String args[]) {
+    int[] A = new int[10];
     Scanner s = new Scanner(System.in);
 
     System.out.print("Enter 10 integers: ");
     for(int i=0;i<10;i++) {
-        z[i] = s.nextInt();
+        A[i] = s.nextInt();
     }
  
-        for (int i = 0; i < 9; i++) {
-            for (int j = i+1; j < 10; j++) {
-                if ((z[i] == z[j]) && (i != j)) {
-                    System.out.println("Duplicate Element : "+z[j]);
-                }
+    for (int i = 0; i < 9; i++) {
+        for (int j = i+1; j < 10; j++) {
+            if ((A[i] == A[j]) && (i != j)) {
+                System.out.println("Duplicate Element : "+A[j]);
             }
         }
-    }    
+    }
+}    
 }
