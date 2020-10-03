21f7aa9c4fd028eb614a92f191071e0dfecd6f38
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-10-03 14:26:36 +0530

    Updated SumOfArray.java program

--- SumOfArray.java
+++ SumOfArray.java
@@ -1,12 +1,13 @@
+//Program to find the sum of elements of an array
 import java.util.*;
 
 class SumOfArray {
 public static void main(String args[]) {
-    int[] a = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int[] A = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     int i,sum = 0;
     for(i=0;i<10;i++) {
-        sum = sum + a[i];
+        sum = sum + A[i];
     }
     System.out.println("Sum of array elements is: "+sum);
-    }
+}
 }
