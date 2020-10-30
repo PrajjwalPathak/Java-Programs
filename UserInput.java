2259cc2acec623571d84f0adbee8c05916ebf4ed
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-10-30 10:07:34 +0530

    Add UserINput.java program

---
+++ UserInput.java
@@ -0,0 +1,16 @@
+//Program to take the input from the user
+
+import java.util.*;
+
+class UserIntput {
+
+    public static void main(String args[]) {
+
+        int a;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        a = s.nextInt();
+        System.out.print("The Number is ");
+        System.out.print(a);
+    }
+}
