9812348f94429f6c40f828c3935c6e174d768057
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-11-21 13:16:41 +0530

    Add Forloop.java

---
+++ Forloop.java
@@ -0,0 +1,13 @@
+//Program to demonstrate Forloop
+
+import java.util.*;
+
+class Forloop {
+    
+    public static void main(String args[]) {
+        int i;
+        for(i=0;i<10;i++) {
+            System.out.println("Spider-Man");
+        }
+    }
+}
